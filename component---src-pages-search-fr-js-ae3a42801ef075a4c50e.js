86b4688fd8784f4fee0140efe2202ad28b11da36
mluukkai <mluukkai@users.noreply.github.com>
2023-08-27 20:20:52 +0000

    deployed via Gatsby Publish Action 🎩 for 4b565e544d81938f129d5afd5f0dbddfa106e2c2

---
+++ component---src-pages-search-fr-js-ae3a42801ef075a4c50e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[49],{vh9U:function(e,n,r){"use strict";r.r(n);var t=r("q1tI"),a=r.n(t),c=r("D9jW");n.default=e=>{let{data:n}=e;return a.a.createElement(c.a,{localSearch:n.localSearchFinnish,title:"Rechercher du contenu",inputPlaceholder:"Entrez un terme de recherche",lang:"fr"})}}}]);
+//# sourceMappingURL=component---src-pages-search-fr-js-ae3a42801ef075a4c50e.js.map
